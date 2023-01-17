48071b931bd7382c02981e4fa34d5cc2e16f2508
Joe Marcus <josephcmarcus@gmail.com>
2023-01-16 20:19:24 -0500

    Build axios request to trigger Paypal processor

--- mv-paypal-process-trigger/index.js
+++ mv-paypal-process-trigger/index.js
@@ -1,3 +1,19 @@
+const dotenv = require('dotenv').config();
+const axios = require('axios');
+
 module.exports = async function (context, myTimer) {
-    
+  try {
+    let date = new Date();
+
+    const response = await axios({
+      method: 'post',
+      url: `${process.env.PAYPAL_PROCESS_SERVICE_URL}?code=${process.env.AUTH_CODE}`,
+      headers: {
+        Accept: '*/*',
+      },
+    });
+    context.log(`Trigger succeeded at ${date}`)
+  } catch (err) {
+    context.log(`Trigger failed at ${date}: ${err}`)
+  }
 };
