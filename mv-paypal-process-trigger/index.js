f20bce3544babccf35f97fdd6996054a288d3f92
Joe Marcus <josephcmarcus@gmail.com>
2023-01-19 00:54:41 -0500

    Fix circular json error

--- mv-paypal-process-trigger/index.js
+++ mv-paypal-process-trigger/index.js
@@ -6,14 +6,17 @@ module.exports = async function (context, myTimer) {
     let date = new Date();
 
     const response = await axios({
-      method: 'post',
+      method: 'get',
       url: `${process.env.PAYPAL_PROCESS_SERVICE_URL}?code=${process.env.AUTH_CODE}`,
       headers: {
         Accept: '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept-Language': 'en_US',
       },
     });
-    context.log(`Trigger succeeded at ${date}. ${response}`);
-    return response;
+    context.log(`Trigger succeeded at ${date}. ${response.data}`);
+    return response.data;
   } catch (err) {
     context.log(`Trigger failed at ${date}: ${err}`)
   }
